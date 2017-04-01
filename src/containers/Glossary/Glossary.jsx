1865d9055edfb6ddd7815d8ab0b36f5eca3c89c7
Caleb Kester <calebkester@gmail.com>
2017-04-01 14:20:50 -0500

    Adding glossary

--- src/containers/Glossary/Glossary.jsx
+++ src/containers/Glossary/Glossary.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import glossaryJSON from './GlossaryData.js';
+import Widget from '../../components/Widget/Widget';
 
 class Glossary extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			glossaryData: glossaryJSON.glossary,
+		};
+	}
+	renderGlossaryItems() {
+		return this.state.glossaryData.map((glossary) => {
+			return (
+				<Widget title={glossary.entry_title}>
+					<div dangerouslySetInnerHTML={{__html: glossary.entry_content }} />
+					{glossary.entry_amendment}
+				</Widget>
+			);
+		});
+	}
 	render() {
 		return (
 			<div>
-				Coming soon
+				{this.renderGlossaryItems()}
 			</div>
 		);
 	}
