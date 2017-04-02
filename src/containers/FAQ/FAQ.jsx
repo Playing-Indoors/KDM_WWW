47c7c1a46d292cf9ef3c20dc3d06da5bbe999140
Caleb Kester <calebkester@gmail.com>
2017-04-01 19:36:36 -0500

    Adding faq

--- src/containers/FAQ/FAQ.jsx
+++ src/containers/FAQ/FAQ.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import { InputGroup, Input, InputGroupButton, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import faqJSON from './faqData.js';
+import Widget from '../../components/Widget/Widget';
 
 class FAQ extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			faqData: faqJSON.entry,
+		};
+	}
+	renderFAQItems() {
+		return this.state.faqData.map((faq) => {
+			return (
+				<Widget title={`${faq.gsx$category.$t} : ${faq.gsx$question.$t}`} key={faq.gsx$id.$t}>
+					{faq.gsx$answer.$t}
+				</Widget>
+			);
+		});
+	}
 	render() {
 		return (
 			<div>
-				FAQ page
+				<Widget>
+					<InputGroup>
+						<Input placeholder="Search FAQ" />
+						<InputGroupButton color="primary">Search</InputGroupButton>
+					</InputGroup>
+				</Widget>
+				{this.renderFAQItems()}
+				<Pagination>
+					<PaginationItem disabled>
+						<PaginationLink previous href="#" />
+					</PaginationItem>
+					<PaginationItem active>
+						<PaginationLink href="#">
+							1
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							2
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							3
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							4
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							5
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink next href="#" />
+					</PaginationItem>
+				</Pagination>
 			</div>
 		);
 	}
--- src/containers/Glossary/Glossary.jsx
+++ src/containers/Glossary/Glossary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputGroup, Input, InputGroupButton } from 'reactstrap';
+import { InputGroup, Input, InputGroupButton, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import glossaryJSON from './GlossaryData.js';
 import Widget from '../../components/Widget/Widget';
 
@@ -11,7 +11,7 @@ class Glossary extends React.Component {
 		};
 	}
 	renderGlossaryItems() {
-		return this.state.glossaryData.map((glossary) => {
+		return this.state.glossaryData.slice(0, 10).map((glossary) => {
 			return (
 				<Widget title={glossary.entry_title} key={glossary.id}>
 					<div dangerouslySetInnerHTML={{__html: glossary.entry_content }} />
@@ -25,11 +25,44 @@ class Glossary extends React.Component {
 			<div>
 				<Widget>
 					<InputGroup>
-						<Input placeholder="Filter glossary" />
+						<Input placeholder="Search glossary" />
 						<InputGroupButton color="primary">Search</InputGroupButton>
 					</InputGroup>
 				</Widget>
 				{this.renderGlossaryItems()}
+				<Pagination>
+					<PaginationItem disabled>
+						<PaginationLink previous href="#" />
+					</PaginationItem>
+					<PaginationItem active>
+						<PaginationLink href="#">
+							1
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							2
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							3
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							4
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">
+							5
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink next href="#" />
+					</PaginationItem>
+				</Pagination>
 			</div>
 		);
 	}
