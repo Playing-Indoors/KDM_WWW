9942c60a1eea0ebad0e6c47099db3aeda45bc950
Khoa Ngo <kaythxs@gmail.com>
2017-04-04 10:02:20 -0700

    updated toggle component and also removed outline from buttons

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -6,25 +6,40 @@ import Icon from '../../components/Icon/Icon';
  * Simple boolean toggle
  * Hitting the button will change it to true or false
  */
-const Toggle = ({ label, active }) => (
-	<div className="toggle">
-		<Row noGutters className="align-items-center">
-			<Col>
-				<div className="toggle-label">{label}</div>
-			</Col>
-			<Col xs="auto">
-				{/* True button */}
-				<Button color="link">
-					<Icon name="checkCircle" color={active ? 'yellow' : 'white'} />
-				</Button>
-				{/* False button */}
-				<Button color="link">
-					<Icon name="closeCircle" color={active ? 'white' : 'yellow'} />
-				</Button>
-			</Col>
-		</Row>
-	</div>
-);
+class Toggle extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleTrue = this.handleTrue.bind(this);
+		this.handleFalse = this.handleFalse.bind(this);
+	}
+	handleTrue(){
+		this.props.updateToggle(true, this.props.for);
+	}
+	handleFalse(){
+		this.props.updateToggle(false, this.props.for);
+	}
+	render(){
+		return(
+			<div className="toggle">
+				<Row noGutters className="align-items-center">
+					<Col>
+						<div className="toggle-label">{this.props.label}</div>
+					</Col>
+					<Col xs="auto">
+						{/* True button */}
+						<Button color="link" onClick={this.handleTrue}>
+							<Icon name="checkCircle" color={this.props.active ? 'yellow' : 'white'} />
+						</Button>
+						{/* False button */}
+						<Button color="link" onClick={this.handleFalse}>
+							<Icon name="closeCircle" color={this.props.active ? 'white' : 'yellow'} />
+						</Button>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+}
 
 Toggle.propTypes = {
 	active: React.PropTypes.bool,
--- src/containers/System/System.jsx
+++ src/containers/System/System.jsx
@@ -3,21 +3,40 @@ import Toggle from '../../components/Toggle/Toggle';
 import Widget from '../../components/Widget/Widget';
 
 class System extends React.Component {
+	constructor(props){
+		super(props);
+		//fakeson not sure what the toggle data struture would look like, but should be flat when in this state
+		this.state = {
+			weapon_specialization: true,
+			settlement_bonuses: false,
+			story_events: true,
+			endeavor_token: false,
+			populate: true,
+			enable_controls: true,
+			log_out: false
+		}
+		this.updateToggle = this.updateToggle.bind(this);
+	}
+	updateToggle(bool, type){
+		this.setState({
+			[type]: bool
+		});
+	}
 	render() {
 		return (
 			<div>
 				<Widget title="Automation">
-					<Toggle active label="Automatically add weapon specilization if innovations include the mastery" />
-					<Toggle label="Automatically apply settlement bonuses to newborn and current survivors where appropriate?" />
-					<Toggle active label="Automatically update timeline with milestone story events?" />
+					<Toggle updateToggle={this.updateToggle} for="weapon_specialization" active={this.state.weapon_specialization} label="Automatically add weapon specilization if innovations include the mastery" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.settlement_bonuses} for="settlement_bonuses" label="Automatically apply settlement bonuses to newborn and current survivors where appropriate?" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.story_events} for="story_events" label="Automatically update timeline with milestone story events?" />
 				</Widget>
 				<Widget title="Settlement Summary">
-					<Toggle label="Show endeavor token control on settlement summary view?" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.endeavor_token} for="endeavor_token" label="Show endeavor token control on settlement summary view?" />
 				</Widget>
 				<Widget title="General">
-					<Toggle active label="Populate random names for new settlements and survivors?" />
-					<Toggle active label="Enable controls for removing settlements and survivors?" />
-					<Toggle label="Automatically log me out after 24 hours of inactivity?" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.populate} for="populate" label="Populate random names for new settlements and survivors?" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.enable_controls} for="enable_controls" label="Enable controls for removing settlements and survivors?" />
+					<Toggle updateToggle={this.updateToggle} active={this.state.log_out} for="log_out" label="Automatically log me out after 24 hours of inactivity?" />
 				</Widget>
 			</div>
 		);
