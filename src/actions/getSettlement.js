80b72c496a947db17c6caf95cc3e64d68e85e6c2
Khoa Ngo <kaythxs@gmail.com>
2017-07-25 05:53:08 -0700

    added auth and change route for settlements

--- src/actions/getSettlement.js
+++ src/actions/getSettlement.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { GET_SETTLEMENT } from './types';
 
 const KDM_API = require('KDM_API');
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('access_token');
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 function getSettlementAsync(data) {
 	return {
@@ -13,7 +15,7 @@ function getSettlementAsync(data) {
 export function getSettlement() {
 	return dispatch => {
 		axios({
-			method: 'post',
+			method: 'get',
 			url: `${KDM_API}/settlement/get/5681f9e7421aa93924b6d013`,
 		}).then((res) => {
 			console.log('SETTLEMENT', res);
