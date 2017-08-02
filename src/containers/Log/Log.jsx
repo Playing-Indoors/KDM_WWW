0e66f9655617cef32182ecd31928b62a181d0b3f
Khoa Ngo <kaythxs@gmail.com>
2017-08-02 06:00:22 -0700

    flowed data into logs

--- src/containers/Log/Log.jsx
+++ src/containers/Log/Log.jsx
@@ -1,26 +1,58 @@
-import React from 'react';
 import { Button, Col, Row } from 'reactstrap';
 import Widget from '../../components/Widget/Widget';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getSettlement } from '../../actions/getSettlement';
 
-class Log extends React.Component {
+class Log extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			campaignName: null,
+			lanternYear: null,
+			population: null,
+			milestones: null,
+			recentMilestone: null,
+			recentDeath: null,
+			recentBirth: null,
+			recentQuarry: null,
+			lastLog: null
+		}
+	}
+	componentWillReceiveProps(nextProps){
+		if(nextProps.settlementData){
+			this.setState({
+				campaignName: nextProps.settlementData.sheet.campaign,
+				lanternYear: nextProps.settlementData.sheet.lantern_year,
+				population: nextProps.settlementData.sheet.population,
+				milestones: nextProps.settlementData.sheet.milestone_story_events.length,
+				recentMilestone: nextProps.settlementData.sheet.milestone_story_events[nextProps.settlementData.sheet.milestone_story_events.length -1],
+				recentDeath: null,
+				recentBirth: null,
+				recentQuarry: nextProps.settlementData.sheet.quarries[nextProps.settlementData.sheet.quarries.length-1],
+				lastLog: null
+			})
+		}
+	}
 	render() {
 		return (
 			<div>
-				<h1>[Campaign Name]</h1>
+				<h1>{this.state.campaignName}</h1>
 				<Row>
 					<Col>
-						<Widget title="Lantern Year">[3]</Widget>
+						<Widget title="Lantern Year">{this.state.lanternYear}</Widget>
 					</Col>
 					<Col>
-						<Widget title="Population">[13]</Widget>
+						<Widget title="Population">{this.state.population}</Widget>
 					</Col>
 					<Col>
-						<Widget title="Milestones">[3]</Widget>
+						<Widget title="Milestones">{this.state.milestones}</Widget>
 					</Col>
 				</Row>
 				<Row>
 					<Col>
-						<Widget title="Most Recent Milestone">[Milestone Name]</Widget>
+						<Widget title="Most Recent Milestone">{this.state.recentMilestone}</Widget>
 					</Col>
 				</Row>
 				<Row>
@@ -35,7 +67,7 @@ class Log extends React.Component {
 				</Row>
 				<Row>
 					<Col>
-						<Widget title="Most Recent Monster Quarry">[Monster Name]</Widget>
+						<Widget title="Most Recent Monster Quarry">{this.state.recentQuarry}</Widget>
 					</Col>
 				</Row>
 				<Row>
@@ -48,4 +80,10 @@ class Log extends React.Component {
 	}
 }
 
-export default Log;
+function mapStateToProps(state) {
+	return {
+		settlementData: state.settlementData,
+	};
+}
+
+export default connect(mapStateToProps, null)(Log);
