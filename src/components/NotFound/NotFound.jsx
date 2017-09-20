e4f3f0e1625c196be50b165500af799363a235e2
Caleb Kester <ckester@coachingactuaries.com>
2017-09-20 09:25:08 -0500

    initial dump

--- src/components/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class NotFound extends Component {
-  render() {
-    return <h3>Page not found.</h3>;
-  }
-}
-
-export default NotFound;
--- src/containers/Storage/Storage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Storage extends React.Component {
-  render() {
-    return <div>Coming soon</div>;
-  }
-}
-
-export default Storage;
