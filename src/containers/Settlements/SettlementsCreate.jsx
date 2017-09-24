cc0a7df79cd1e0f3829e56de6de09d75f652e8cb
Caleb Kester <calebkester@gmail.com>
2017-09-23 21:41:42 -0500

    More create settlement work

--- src/containers/Settlements/SettlementsCreate.jsx
+++ src/containers/Settlements/SettlementsCreate.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {
-  Label,
   Button,
   Input,
+  FormGroup,
   Nav,
   NavItem,
   NavLink,
@@ -22,7 +22,7 @@ class Settlements extends React.Component {
     this.toggle = this.toggle.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      activeTab: "1"
+      activeTab: 1
     };
   }
   toggle(tab) {
@@ -34,7 +34,13 @@ class Settlements extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    console.log("Next page");
+    if (this.state.activeTab < 4) {
+      this.setState({
+        activeTab: this.state.activeTab + 1
+      });
+    } else {
+      console.warn("KHOA CREATE SETTLEMENT!");
+    }
   }
   render() {
     return (
@@ -44,55 +50,137 @@ class Settlements extends React.Component {
             <Icon name={"check"} />
           </span>
         </Header>
-        <div className="layout">
+        <div className="layoutContent">
           <Nav tabs>
             <NavItem>
               <NavLink
-                className={classnames({ active: this.state.activeTab === "1" })}
+                tabIndex="0"
+                className={classnames({ active: this.state.activeTab === 1 })}
                 onClick={() => {
-                  this.toggle("1");
+                  this.toggle(1);
                 }}
               >
-                Tab1
+                Name
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink
-                className={classnames({ active: this.state.activeTab === "2" })}
+                tabIndex="0"
+                className={classnames({ active: this.state.activeTab === 2 })}
                 onClick={() => {
-                  this.toggle("2");
+                  this.toggle(2);
                 }}
               >
-                Moar Tabs
+                Campaign
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                tabIndex="0"
+                className={classnames({ active: this.state.activeTab === 3 })}
+                onClick={() => {
+                  this.toggle(3);
+                }}
+              >
+                Expansions
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                tabIndex="0"
+                className={classnames({ active: this.state.activeTab === 4 })}
+                onClick={() => {
+                  this.toggle(4);
+                }}
+              >
+                Survivors
               </NavLink>
             </NavItem>
           </Nav>
           <TabContent activeTab={this.state.activeTab}>
-            <TabPane tabId="1">111</TabPane>
-            <TabPane tabId="2">sdfsdfsdf</TabPane>
+            <TabPane tabId={1}>
+              <form onSubmit={this.handleSubmit}>
+                <legend>Name the Settlement</legend>
+                <Widget>
+                  <Input
+                    type="text"
+                    name="name"
+                    placeholder="Enter settlement name..."
+                    size="sm"
+                    autoFocus
+                    required
+                  />
+                  <WidgetFooter>
+                    <Button color="gray" size="sm">
+                      Randomize Name
+                    </Button>
+                    <Button color="primary" size="sm" type="submit">
+                      Confirm
+                    </Button>
+                  </WidgetFooter>
+                </Widget>
+              </form>
+            </TabPane>
+            <TabPane tabId={2}>
+              <form onSubmit={this.handleSubmit}>
+                <legend>Select Campaign</legend>
+                <Widget>
+                  <FormGroup>
+                    <Input
+                      type="select"
+                      name="selectMulti"
+                      id="exampleSelectMulti"
+                      size="sm"
+                    >
+                      <option>People of the Lantern</option>
+                      <option>People of the Stars</option>
+                      <option>People of the Sun</option>
+                    </Input>
+                  </FormGroup>
+                  <WidgetFooter>
+                    <Button color="primary" size="sm" type="submit">
+                      Confirm
+                    </Button>
+                  </WidgetFooter>
+                </Widget>
+              </form>
+            </TabPane>
+            <TabPane tabId={3}>
+              <form onSubmit={this.handleSubmit}>
+                <legend>Select Expansions</legend>
+                <Widget>
+                  <WidgetFooter>
+                    <Button color="primary" size="sm" type="submit">
+                      Confirm
+                    </Button>
+                  </WidgetFooter>
+                </Widget>
+              </form>
+            </TabPane>
+            <TabPane tabId={4}>
+              <form onSubmit={this.handleSubmit}>
+                <legend>Create Starting Survivors</legend>
+                <Widget>
+                  <Input
+                    type="text"
+                    name="name"
+                    placeholder="Enter settlement name..."
+                    size="sm"
+                    autoFocus
+                    required
+                  />
+                  <WidgetFooter>
+                    <Button color="gray" size="sm">
+                      Randomize Name
+                    </Button>
+                    <Button color="primary" size="sm" type="submit">
+                      Confirm
+                    </Button>
+                  </WidgetFooter>
+                </Widget>
+              </form>
+            </TabPane>
           </TabContent>
-          <form onSubmit={this.handleSubmit}>
-            {/* TODO: Make this a form */}
-            <Label>Name the Settlement</Label>
-            <Widget>
-              <Input
-                type="text"
-                name="name"
-                placeholder="Enter settlement name..."
-                size="sm"
-                autoFocus
-                required
-              />
-              <WidgetFooter>
-                <Button color="gray" size="sm">
-                  Randomize Name
-                </Button>
-                <Button color="primary" size="sm" type="submit">
-                  Confirm
-                </Button>
-              </WidgetFooter>
-            </Widget>
-          </form>
         </div>
       </div>
     );
