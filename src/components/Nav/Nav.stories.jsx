a61d4a2e8e7cc132b6c9d64adf861c3eb973df07
Caleb Kester <ckester@coachingactuaries.com>
2017-10-04 11:05:47 -0500

    storybook

--- src/components/Nav/Nav.stories.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react"; // eslint-disable-line
-import Nav from "./Nav";
-
-storiesOf("Nav", module).add("with text", () => <Nav />);
