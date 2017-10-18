2e85fff45cdd04422d96ae69d98feecd568168f1
Caleb Kester <calebkester@gmail.com>
2017-10-17 23:17:55 -0500

    survivor create

--- src/containers/Survivors/SurvivorsCreate.jsx
+++ src/containers/Survivors/SurvivorsCreate.jsx
@@ -7,7 +7,8 @@ import {
   NavItem,
   NavLink,
   TabContent,
-  TabPane
+  TabPane,
+  ButtonGroup
 } from "reactstrap";
 import { Link } from "react-router";
 import { connect } from "react-redux";
@@ -26,6 +27,8 @@ class Settlements extends React.Component {
 
     this.handleCreate = this.handleCreate.bind(this);
     this.randomName = this.randomName.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleGenderChange = this.handleGenderChange.bind(this);
     this.state = {
       name: "",
       gender: "F"
@@ -71,9 +74,9 @@ class Settlements extends React.Component {
   }
   handleCreate(e) {
     e.preventDefault();
-    let settlementId = window.location.pathname.split("/")[2];
+    const settlementId = window.location.pathname.split("/")[2];
     createSurvivor(settlementId, this.state)
-      .then(res => {
+      .then(() => {
         this.props.getSettlement();
         browserHistory.goBack();
       })
@@ -91,26 +94,24 @@ class Settlements extends React.Component {
       gender: e.target.value
     });
   }
-  renderCreate() {
-    if (this.state.name.length > 0) {
-      return (
-        <Link to={"./"} onClick={this.handleCreate} className="header-action">
-          <Icon name={"check"} color="yellow" />
-        </Link>
-      );
-    }
-    return (
-      <span className="header-action">
-        <Icon name={"check"} />
-      </span>
-    );
-  }
+  // renderCreate() {
+  //   if (this.state.name.length > 0) {
+  //     return (
+  //       <Link to={"./"} onClick={this.handleCreate} className="header-action">
+  //         <Icon name={"check"} color="yellow" />
+  //       </Link>
+  //     );
+  //   }
+  //   return (
+  //     <span className="header-action">
+  //       <Icon name={"check"} />
+  //     </span>
+  //   );
+  // }
   render() {
     return (
       <div>
-        <Header name={"Create Survivor"} showBack>
-          {this.renderCreate()}
-        </Header>
+        <Header name={"Create Survivor"} />
         <form className="layout" onSubmit={this.handleCreate}>
           <legend>Create Survivor</legend>
           <Widget>
@@ -122,7 +123,7 @@ class Settlements extends React.Component {
               size="sm"
               autoFocus
               required
-              onChange={this.handleNameChange.bind(this)}
+              onChange={this.handleNameChange}
             />
             <WidgetFooter>
               <Button color="gray" size="sm" onClick={this.randomName}>
@@ -132,12 +133,22 @@ class Settlements extends React.Component {
                 type="select"
                 value={this.state.gender}
                 size="sm"
-                onChange={this.handleGenderChange.bind(this)}
+                onChange={this.handleGenderChange}
               >
                 <option value="F">Female</option>
                 <option value="M">Male</option>
               </Input>
             </WidgetFooter>
+          </Widget>
+          <Widget>
+            <ButtonGroup className="btn-group--full">
+              <Button color="gray" block>
+                Cancel
+              </Button>
+              <Button color="primary" block type="submit">
+                Create
+              </Button>
+            </ButtonGroup>
           </Widget>
         </form>
       </div>
