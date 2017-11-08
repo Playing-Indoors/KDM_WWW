19ef657ea51dcc9f3a25fd6332ce40f324625b7e
Khoa Ngo <kaythxs@gmail.com>
2017-11-08 07:51:23 -0800

    did something with login

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,6 +1,12 @@
 import { browserHistory } from "react-router";
 import axios from "axios";
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, GET_USER } from "./types";
+import {
+  AUTH_USER,
+  AUTH_ERROR,
+  UNAUTH_USER,
+  GET_USER,
+  GET_SETTLEMENT
+} from "./types";
 const KDM_API = require("KDM_API");
 
 export function authError(error) {
@@ -10,18 +16,31 @@ export function authError(error) {
   };
 }
 export function authenticate({ username, password }) {
-  console.log('hit auth');
   return function(dispatch) {
-    axios.defaults.headers.common["Content-Type"] = "application/json";
     axios
       .post(`${KDM_API}/login`, { username: username, password: password })
       .then(response => {
         localStorage.setItem("access_token", response.data.access_token);
         localStorage.setItem("userId", response.data._id);
+        let auth = response.data.access_token;
+        let userId = response.data._id;
         dispatch({ type: AUTH_USER });
-        browserHistory.push(
-          `/settlements/`
-        );
+        axios({
+          headers: { Authorization: auth },
+          method: "get",
+          url: `${KDM_API}/user/dashboard/${userId}`
+        }).then(res => {
+          let settlementId = res.data.user.current_settlement.$oid;
+          dispatch(getUserAsync(res.data));
+          axios({
+            headers: { Authorization: auth },
+            method: "get",
+            url: `${KDM_API}/settlement/get/${settlementId}`
+          }).then(res => {
+            dispatch(getSettlementAsync(res.data));
+            browserHistory.push(`/settlements/${settlementId}`);
+          });
+        });
       })
       .catch(err => {
         console.log("Error:", err);
@@ -33,4 +52,18 @@ export function authenticate({ username, password }) {
 export function signoutUser() {
   localStorage.removeItem("access_token");
   return { type: UNAUTH_USER };
+}
+
+function getUserAsync(data) {
+  return {
+    type: GET_USER,
+    payload: data
+  };
+}
+
+function getSettlementAsync(data) {
+  return {
+    type: GET_SETTLEMENT,
+    payload: data
+  };
 }
