ffa4a8c0c48bcd55681ed8e5a88b1f207ee01329
Khoa Ngo <kaythxs@gmail.com>
2017-11-08 08:00:28 -0800

    updated app calls

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    this.props.getUser();
+    if (localStorage.getItem("userId")) {
+      this.props.getUser();
+    }
   }
   showBack() {
     if (this.props.routes[3]) {
