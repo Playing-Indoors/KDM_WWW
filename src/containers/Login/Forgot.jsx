cef78cc17bd997d4794deccf07dd25854338cafb
Caleb Kester <ckester@coachingactuaries.com>
2017-12-18 13:58:20 -0600

    Quick fix to prevent more js errors

--- src/containers/Login/Forgot.jsx
+++ src/containers/Login/Forgot.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router";
 import { Button, FormGroup, Label, Input } from "reactstrap";
 import { passwordReset } from "../../actions/passwordReset";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 class Forgot extends Component {
   constructor(props) {
@@ -9,7 +10,8 @@ class Forgot extends Component {
     this.state = {
       email: "",
       loading: false,
-      error: false
+      messageSent: false,
+      error: ""
     };
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -21,10 +23,43 @@ class Forgot extends Component {
   }
   handleFormSubmit(e) {
     e.preventDefault();
-    passwordReset({ username: this.state.email }).then(res => {
-      console.log("Password email sent", res);
-      alert("Password email sent");
-    });
+    this.setState({ loading: true });
+    passwordReset({ username: this.state.email })
+      .then(() => {
+        this.setState({
+          loading: false,
+          error: false,
+          messageSent: true
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false,
+          error: err.data
+        });
+      });
+  }
+  renderError() {
+    if (this.state.error.length > 0) {
+      return <div className="tw-bg-red tw-p-4 tw-mb-4">{this.state.error}</div>;
+    }
+    return null;
+  }
+  renderMessage() {
+    if (this.state.messageSent) {
+      return (
+        <div className="tw-bg-green tw-p-4 tw-mt-4 tw-mb-4">
+          Email Sent! Please check your inbox.
+        </div>
+      );
+    }
+    return null;
+  }
+  renderLoading() {
+    if (this.state.loading) {
+      return <LoadingSpinner absolute />;
+    }
+    return null;
   }
   render() {
     return (
@@ -36,6 +71,7 @@ class Forgot extends Component {
           />
         </div>
         <form className="login-form" onSubmit={this.handleFormSubmit}>
+          {this.renderLoading()}
           <FormGroup className="input-form--dark">
             <Label for="email">Enter your email</Label>
             <Input
@@ -49,6 +85,7 @@ class Forgot extends Component {
               required
             />
           </FormGroup>
+          {this.renderError()}
           <Button
             type="submit"
             color="primary"
@@ -57,6 +94,7 @@ class Forgot extends Component {
           >
             Send Reset Email
           </Button>
+          {this.renderMessage()}
           <Link to={"/"} className="linkGroup mt-3">
             Back to login
           </Link>
